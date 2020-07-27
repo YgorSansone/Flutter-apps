4884d0c08b4a6f35b5affa3741e59d8f47d96196
ygorsansone <ygorsansone@gmail.com>
2020-07-27 20:14:23 -0300

    ToDoList - com a data atual

--- ToDoList/todolist/test/widget_test.dart
+++ ToDoList/todolist/test/widget_test.dart
@@ -11,9 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:todolist/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Teste', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TodoListApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
