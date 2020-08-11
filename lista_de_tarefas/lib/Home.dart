6f36080fa3c0aab74a7dbbe6bea21db39ea28a5b
Ygor Sansone <ygorsansone@gmail.com>
2020-08-11 20:58:34 -0300

    Salvar dados com sistema de arquivo com PATH para IOS e Android

--- lista_de_tarefas/lib/Home.dart
+++ lista_de_tarefas/lib/Home.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  List _listaTarefas = ["Ir ao mercado","Estudar"];
+  List _listaTarefas = [];
+  _salvarArquivo()async{
+    final diretorio = await getApplicationDocumentsDirectory();
+    var arquivo = File(diretorio.path+"/dados.json");
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = "Ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+  _removerArquivo()async{}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +73,7 @@ class _HomeState extends State<Home> {
                       FlatButton(
                         child: Text("Salvar"),
                         onPressed: (){
-
+                          _salvarArquivo();
                           Navigator.pop((context));
                         },
                       ),
