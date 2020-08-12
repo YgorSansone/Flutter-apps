53a02e4a44c61b1516ae17ff80b4ec8da4546f74
Ygor Sansone <ygorsansone@gmail.com>
2020-08-11 22:18:28 -0300

    Gerenciador de tarefas

---
+++ lista_de_tarefas/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
