15a43114381f997cebbd5426aba4976e2565bca0
Ygor Sansone <ygorsansone@gmail.com>
2020-08-11 21:11:53 -0300

    Ler arquivo salvo

--- lista_de_tarefas/lib/Home.dart
+++ lista_de_tarefas/lib/Home.dart
@@ -11,20 +11,43 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _listaTarefas = [];
-  _salvarArquivo()async{
+  Future<File> _getFile() async{
     final diretorio = await getApplicationDocumentsDirectory();
-    var arquivo = File(diretorio.path+"/dados.json");
+    return File(diretorio.path+"/dados.json");
+  }
+  _salvarArquivo()async{
+    var arquivo = await _getFile();
     Map<String, dynamic> tarefa = Map();
+
     tarefa["titulo"] = "Ir ao mercado";
     tarefa["realizada"] = false;
     _listaTarefas.add(tarefa);
+
     String dados = json.encode(_listaTarefas);
     arquivo.writeAsString(dados);
   }
+  _lerArquivo() async{
+    try{
+      final arquivo =await _getFile();
+      return arquivo.readAsString();
+    }catch(e){
+      return null;
+    }
+  }
   _removerArquivo()async{}
+  @override
+  void initState() {
+    super.initState();
+    _lerArquivo().then((dados){
+      setState(() {
+        _listaTarefas = jsonDecode(dados);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("items: "+ _listaTarefas.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tarefas",style: TextStyle(
@@ -39,7 +62,7 @@ class _HomeState extends State<Home> {
               itemCount: _listaTarefas.length,
               itemBuilder: (context, index){
                 return ListTile(
-                  title: Text(_listaTarefas[index]),
+                  title: Text(_listaTarefas[index]['titulo']),
                 );
               },
             ),
