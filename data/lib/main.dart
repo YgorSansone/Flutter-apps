5e3499f2ae5e36477ff50780b5bdf6aaec07a03f
Ygor Sansone <ygorsansone@gmail.com>
2020-08-12 21:04:55 -0300

    Criar banco de dados IOS e ANDROID dentro do celular

---
+++ data/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+  )
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  _recuperarBancoDeDados() async{
+    final caminhoBancoDados = await getDatabasesPath();
+    final localBancoDados = join(caminhoBancoDados, "banco.db");
+    var retorno = await openDatabase(
+        localBancoDados,
+        version: 1,
+        onCreate:(db, dbVersaoRecente){
+          String sql = "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
+          db.execute(sql);
+        }
+    );
+    print("aberto" + retorno.isOpen.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _recuperarBancoDeDados();
+    return Container();
+  }
+}
