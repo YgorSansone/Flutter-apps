13e324f40f0f5cd6eda9dbc235085521b5d73a7a
Ygor Sansone <ygorsansone@gmail.com>
2020-08-19 21:29:57 -0300

    play - stop - pause em audios do assets

--- sons/lib/main.dart
+++ sons/lib/main.dart
@@ -14,6 +14,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   AudioPlayer audioPlayer = AudioPlayer();
   AudioCache audioCache = AudioCache(prefix: "audios/");
+  bool primeiraExecucao = true;
   _executar() async{
 //    String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-16.mp3";
 //    int resultado = await audioPlayer.play(url);
@@ -22,12 +23,64 @@ class _HomeState extends State<Home> {
 //    }else{
 //      print("erro");
 //    }
+  if(primeiraExecucao){
     audioPlayer = await audioCache.play("musica.mp3");
+    primeiraExecucao = false;
+  }else{
+     audioPlayer.resume();
+  }
+
+  }
+  _parar() async{
+    int resultado = await audioPlayer.pause();
+  }
+  _pausar()async{
+    int resultado = await audioPlayer.stop();
   }
   @override
   Widget build(BuildContext context) {
-    _executar();
-    return Container();
+//    _executar();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Executando sons"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                  padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/imagens/executar.png"),
+                  onTap: (){
+                    _executar();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/imagens/pausar.png"),
+                  onTap: (){
+                    _pausar();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/imagens/parar.png"),
+                  onTap: (){
+                    _parar();
+                  },
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
