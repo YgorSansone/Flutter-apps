9ecb6337d66647c311ddda3180f7af37c442be44
Ygor Sansone <ygorsansone@gmail.com>
2020-08-20 18:26:37 -0300

    Abas AppBar

---
+++ tabbar/lib/paginas/primeira.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Primeira extends StatefulWidget {
+  @override
+  _PrimeiraState createState() => _PrimeiraState();
+}
+
+class _PrimeiraState extends State<Primeira> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Primeira"),
+    );
+  }
+}
