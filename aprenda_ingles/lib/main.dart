d796c2616876e9753c05c690e3412d8f58df80ff
Ygor Sansone <ygorsansone@gmail.com>
2020-08-20 18:58:45 -0300

    Interface do app

--- aprenda_ingles/lib/main.dart
+++ aprenda_ingles/lib/main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'Home.dart';
+void main(){
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Color(0xff795548),
+//      accentColor: Colors.deepOrange,
+      scaffoldBackgroundColor: Color(0xfff5e9b9),
+    ),
+  ));
+}
