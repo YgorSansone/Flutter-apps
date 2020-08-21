93094ddf0ff59c9cc400d7aa9e08b481835839f1
Ygor Sansone <ygorsansone@gmail.com>
2020-08-20 22:38:45 -0300

    Videos por URL

--- video/lib/main.dart
+++ video/lib/main.dart
@@ -11,8 +11,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  VideoPlayerController _controller;
+  @override
+  void initState(){
+    super.initState();
+    _controller = VideoPlayerController.network(
+        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
+    )..initialize().then((_) {
+      setState(() {
+       _controller.play();
+      });
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller),
+        ),
+      ),
+    );
   }
 }
