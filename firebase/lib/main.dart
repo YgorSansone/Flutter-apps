80717d0d05757ea107d004f743940e5476881e51
Ygor Sansone <ygorsansone@gmail.com>
2020-08-21 19:29:08 -0300

    CRUD com listen - REAL TIME

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore db = Firestore.instance;
+
 //    db.collection("usuarios")
 //  .document("001")
 //  .setData(
@@ -19,17 +19,38 @@ void main()  async {
 //    "titulo" : "Novo planeta",
 //    "descricao" : "texto exemplo ... "
 //  });
-  db.collection("noticias")
-  .document("B0eWllFN76cM4cas2NVO")
-  .setData(
-    {
-      "titulo" : "Novo planeta alterado",
-    "descricao" : "texto exemplo ... "
-    }
-  );
 
+//  db.collection("noticias")
+//  .document("B0eWllFN76cM4cas2NVO")
+//  .setData(
+//    {
+//      "titulo" : "Novo planeta alterado",
+//    "descricao" : "texto exemplo ... "
+//    }
+//  );
 
 //  print("Item salvo : " + ref.documentID);
+  Firestore db = Firestore.instance;
+//  db.collection("usuarios").document("003").delete();
+
+//  DocumentSnapshot snapshot = await db.collection("usuarios").document("002").get();
+//  var dados = snapshot.data;
+//  print("Dados : " + dados["Nome"]);
+
+//  QuerySnapshot querySnapshot = await db.collection("usuarios").getDocuments();
+//  print("dados usuarios: " + querySnapshot.documents.toString());
+//  for(DocumentSnapshot item in querySnapshot.documents){
+//    print("dados usuarios: " + item.data.toString());
+//  }
+  db.collection("usuarios").snapshots().listen(
+      (snapshot){
+          for(DocumentSnapshot item in snapshot.documents){
+          print("dados usuarios: " + item.data.toString());
+        }
+        
+      }
+  );
+
   runApp(
       App()
   );
