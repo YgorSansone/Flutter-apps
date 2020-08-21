6ab5c3f446e9c11cf9ecd36d54cc2eb40674f3b5
Ygor Sansone <ygorsansone@gmail.com>
2020-08-20 22:48:25 -0300

    Videos por assets

--- video/lib/main.dart
+++ video/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -13,25 +15,45 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   VideoPlayerController _controller;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
-        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
-    )..initialize().then((_) {
-      setState(() {
-       _controller.play();
+
+//    _controller = VideoPlayerController.network(
+//        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
+//    )..initialize().then((_) {
+//      setState(() {
+//       _controller.play();
+//      });
+//    });
+    _controller = VideoPlayerController.asset("videos/exemplo.mp4")
+      ..setLooping(true)
+      ..initialize().then((_) {
+        setState(() {
+          //      _controller.play();
+        });
       });
-    });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-          child: VideoPlayer(_controller),
+        child: _controller.value.initialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Text("Pressione play"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
+        onPressed: (){
+          setState(() {
+            _controller.value.isPlaying ? _controller.pause() : _controller.play();
+          });
+        },
       ),
     );
   }
