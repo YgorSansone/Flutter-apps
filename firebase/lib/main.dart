32044c705fd1330f0999608fd150d7f5225e3f2f
Ygor Sansone <ygorsansone@gmail.com>
2020-08-23 21:08:13 -0300

    Cadastrar usuario e pegar dados do usuario

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
-
-
 //    db.collection("usuarios")
 //  .document("001")
 //  .setData(
@@ -55,24 +53,46 @@ void main()  async {
 //  );
 
 
-  var pesquisa = "Mar";
-  Firestore db = Firestore.instance;
-  QuerySnapshot querySnapshot = await db.collection("usuarios")
+//  var pesquisa = "Mar";
+//  Firestore db = Firestore.instance;
+//  QuerySnapshot querySnapshot = await db.collection("usuarios")
 //      .where("Nome", isEqualTo: "ygor")
 //  .where("idade", isGreaterThan: 15)
 //  .where("idade", isLessThan: 50)
 //  .orderBy("idade", descending: true)
 //  .orderBy("Nome", descending: false)
   //  .limit(2)
-  .where("Nome", isGreaterThanOrEqualTo: pesquisa)
-  .where("Nome", isLessThanOrEqualTo: pesquisa + "\uf8ff")
-      .getDocuments();
+//  .where("Nome", isGreaterThanOrEqualTo: pesquisa)
+//  .where("Nome", isLessThanOrEqualTo: pesquisa + "\uf8ff")
+//      .getDocuments();
+//
+//  for(DocumentSnapshot item in querySnapshot.documents){
+//    var dados = item.data;
+//    print("filtro : "+ dados["Nome"] +" _ "+ dados["idade"].toString());
+//  }
 
-  for(DocumentSnapshot item in querySnapshot.documents){
-    var dados = item.data;
-    print("filtro : "+ dados["Nome"] +" _ "+ dados["idade"].toString());
+  ////////////////////////////////////////////////////////////////////
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String email = "ygorsansone@gmail.com";
+  String senha = "123456";
+
+  //CADASTRO
+//  auth.createUserWithEmailAndPassword(
+//      email: email,
+//      password: senha
+//  ).then((firebaseUser){
+//    print("Sucesso: email: " +firebaseUser.email);
+//  }).catchError((erro){
+//    print("ERRO : " +erro.toString());
+//    });
+
+  FirebaseUser usuarioAtual = await auth.currentUser();
+  if(usuarioAtual != null){
+    print("logado : " +usuarioAtual.email);
+  }else{
+    print("deslogado");
   }
-
+  
   runApp(
       App()
   );
