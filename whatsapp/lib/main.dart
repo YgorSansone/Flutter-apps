e687427038af34205c5f6fc3849843fb2197cb9d
Ygor Sansone <ygorsansone@gmail.com>
2020-08-24 21:12:09 -0300

    WhatsApp

---
+++ whatsapp/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'Home.dart';
+
+void main() {
+  Firestore.instance
+  .collection("usuarios")
+  .document("001")
+  .setData({"nome" : "ygor"});
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
