3e85e527e21ba7898bf530df70b36fc893a1eed7
Ygor Sansone <ygorsansone@gmail.com>
2020-09-04 21:28:25 -0300

    Configuracoes iniciais

---
+++ maps/ios/Runner/AppDelegate.m
@@ -0,0 +1,15 @@
+#import "AppDelegate.h"
+#import "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyD0QjweZ7NGWknDvwxMR531F9tVWYxfe5g"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
---
+++ maps/lib/Home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Completer<GoogleMapController> _controller = Completer();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Mapas"),),
+      body: Container(
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(-23.562436, -46.655005),
+            zoom: 16
+          ),
+          onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
+}
