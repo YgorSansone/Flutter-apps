ec7db35e9583229641ca01aa729a35ab90388cf9
Ygor Sansone <ygorsansone@gmail.com>
2020-09-04 22:10:24 -0300

    Recursos do mapa

--- maps/lib/Home.dart
+++ maps/lib/Home.dart
@@ -11,12 +11,29 @@ class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
   _onMapCreated(GoogleMapController googleMapController){
     _controller.complete(googleMapController);
-
+  }
+  _movimentarCamera() async{
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(-23.962436, -46.655005),
+            zoom: 19,
+          tilt: 45,
+          bearing: 270
+        )
+      )
+    );
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Mapas"),),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _movimentarCamera,
+        child: Icon(Icons.done),
+      ),
       body: Container(
         child: GoogleMap(
           mapType: MapType.satellite,
