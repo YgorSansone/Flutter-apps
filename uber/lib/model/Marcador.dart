2e87d8077eabc90f9fac929714058926f02a5015
Ygor Sansone <ygorsansone@gmail.com>
2020-09-29 20:43:27 -0300

    Add classe Marcador

---
+++ uber/lib/model/Marcador.dart
@@ -0,0 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Marcador{
+  LatLng local;
+  String camingoImagem;
+  String titulo;
+
+  Marcador(this.local, this.camingoImagem, this.titulo);
+}
