6365e30200c80e42fef87cdb0d154072ec948379
Ygor Sansone <ygorsansone@gmail.com>
2020-09-28 22:20:19 -0300

    Add atualizarDadosLocalizacao

--- uber/lib/util/UsuarioFirebase.dart
+++ uber/lib/util/UsuarioFirebase.dart
@@ -24,4 +24,15 @@ class UsuarioFirebase{
     usuario.nome = nome;
     return usuario;
   }
+  static atualizarDadosLocalizacao(String idRequisicao, double lat, double lon)async{
+    Firestore db = Firestore.instance;
+    Usuario motorista= await getDadosUsuarioLogado();
+    motorista.latitude = lat;
+    motorista.longitude = lon;
+    db.collection("requisicoes")
+    .document(idRequisicao)
+    .updateData({
+      "motorista": motorista.toMap()
+    });
+  }
 }
