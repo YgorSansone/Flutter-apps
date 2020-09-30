b4946802fc2212c7fd4c502af084183259acb19b
Ygor Sansone <ygorsansone@gmail.com>
2020-09-29 21:50:10 -0300

    Refatorando

--- uber/lib/model/Marcador.dart
+++ uber/lib/model/Marcador.dart
@@ -2,8 +2,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Marcador{
   LatLng local;
-  String camingoImagem;
+  String caminhoImagem;
   String titulo;
 
-  Marcador(this.local, this.camingoImagem, this.titulo);
+  Marcador(this.local, this.caminhoImagem, this.titulo);
 }
