f15322619d1e21d6e65cd77e4197016d3863297e
Ygor Sansone <ygorsansone@gmail.com>
2020-09-29 21:50:54 -0300

    verifica o tipo do usuario

--- uber/lib/util/UsuarioFirebase.dart
+++ uber/lib/util/UsuarioFirebase.dart
@@ -1,38 +1,55 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uber/model/Usuario.dart';
-class UsuarioFirebase{
-  static Future<FirebaseUser> getUsuarioAtual() async{
+
+class UsuarioFirebase {
+
+  static Future<FirebaseUser> getUsuarioAtual() async {
+
     FirebaseAuth auth = FirebaseAuth.instance;
     return await auth.currentUser();
+
   }
-  static Future<Usuario> getDadosUsuarioLogado() async{
+
+  static Future<Usuario> getDadosUsuarioLogado() async {
+
     FirebaseUser firebaseUser = await getUsuarioAtual();
     String idUsuario = firebaseUser.uid;
+
     Firestore db = Firestore.instance;
+
     DocumentSnapshot snapshot = await db.collection("usuarios")
-    .document(idUsuario)
-    .get();
+        .document( idUsuario )
+        .get();
+
     Map<String, dynamic> dados = snapshot.data;
     String tipoUsuario = dados["tipoUsuario"];
     String email = dados["email"];
     String nome = dados["nome"];
+
     Usuario usuario = Usuario();
     usuario.idUsuario = idUsuario;
-    usuario.tipoUsuario =tipoUsuario;
+    usuario.tipoUsuario = tipoUsuario;
     usuario.email = email;
     usuario.nome = nome;
+
     return usuario;
+
   }
-  static atualizarDadosLocalizacao(String idRequisicao, double lat, double lon)async{
+
+  static atualizarDadosLocalizacao(String idRequisicao, double lat, double lon) async {
+
     Firestore db = Firestore.instance;
-    Usuario usuario= await getDadosUsuarioLogado();
-    usuario.latitude = lat;
-    usuario.longitude = lon;
+
+    Usuario motorista = await getDadosUsuarioLogado();
+    motorista.latitude = lat;
+    motorista.longitude = lon;
+    String tipo = motorista.tipoUsuario.toString() == "motorista" ? "motorista" : "passageiro";
     db.collection("requisicoes")
-    .document(idRequisicao)
-    .updateData({
-      "motorista": usuario.toMap()
+        .document( idRequisicao )
+        .updateData({
+      "${tipo}" : motorista.toMap()
     });
+
   }
 }
