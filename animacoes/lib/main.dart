fba28df1ad9f1fde3c0909063569b0df6d7b93d0
Ygor Sansone <ygorsansone@gmail.com>
2020-09-30 21:33:53 -0300

    Animacoes

---
+++ animacoes/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'AnimacaoImplicita.dart';
+import 'CriandoAnimacoesBasicas.dart';
+
+void main() {
+runApp(  MaterialApp(
+  home: CriandoAnimacoesBasicas(),
+  debugShowCheckedModeBanner: false,
+));
+}
