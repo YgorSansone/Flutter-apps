8ce60b47ae2655d59b3667b020076600120f3734
Ygor Sansone <ygorsansone@gmail.com>
2020-10-01 23:10:03 -0300

    Add animacoesExplicitasContruidas

--- animacoes/lib/main.dart
+++ animacoes/lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'AnimacaoImplicita.dart';
 import 'AnimacaoTween.dart';
+import 'AnimacoesExplicitasContruidas.dart';
 import 'CriandoAnimacoesBasicas.dart';
 
 void main() {
 runApp(  MaterialApp(
-  home: AnimacaoTween(),
+  home: AnimacoesExplicitasConstruidas(),
   debugShowCheckedModeBanner: false,
 ));
 }
