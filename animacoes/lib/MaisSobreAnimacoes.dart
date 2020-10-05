3dcc4a249d253e100c676f14509cde9ab5b87f1d
Ygor Sansone <ygorsansone@gmail.com>
2020-10-05 20:44:17 -0300

    add animacoes de zoom na imagem

---
+++ animacoes/lib/MaisSobreAnimacoes.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+class MaisSobreAnimacoes extends StatefulWidget {
+  @override
+  _MaisSobreAnimacoesState createState() => _MaisSobreAnimacoesState();
+}
+
+class _MaisSobreAnimacoesState extends State<MaisSobreAnimacoes> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _animation;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: 1
+    ).animate(_animationController);
+  }
+  @override
+  Widget build(BuildContext context) {
+    _animationController.forward();
+    return Container(
+      padding: EdgeInsets.all(20),
+      color: Colors.white,
+      child: AnimatedBuilder(
+        animation: _animation,
+        child: Image.asset("imagens/logo.png"),
+        builder: (contex, widget){
+
+          // return Transform.rotate(
+          //     angle: _animation.value,
+          //         child: widget,
+          // );
+
+          return Transform.scale(
+            scale: _animation.value,
+            child: widget,
+          );
+
+        },
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
