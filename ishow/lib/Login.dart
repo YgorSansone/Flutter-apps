c21453ff4addbda6c5e0482abc94997dd5924d62
Ygor Sansone <ygorsansone@gmail.com>
2020-10-05 21:11:23 -0300

    Interfaces de login

---
+++ ishow/lib/Login.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("imagens/fundo.png"),
+                    fit: BoxFit.fill
+                  )
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 10,
+                      child: Image.asset("imagens/detalhe1.png"),
+                    ),
+                    Positioned(
+                      left: 50,
+                      child: Image.asset("imagens/detalhe2.png"),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 30,right: 30),
+                child: Column(children: [
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey[200],
+                          blurRadius: 15,
+                          spreadRadius: 4
+                        )
+                      ]
+                    ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: "Email"
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  // InkWell(),
+                  SizedBox(height: 10,),
+                  Text("Esqueci a minha senha", style: TextStyle(
+                    color: Color.fromRGBO(255, 100, 127, 1),
+                    fontWeight: FontWeight.bold
+                  ),)
+                ],),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- ishow/lib/main.dart
+++ ishow/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ishow/Login.dart';
 
 void main() {
-
+runApp(
+  MaterialApp(
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  )
+);
 }
 
