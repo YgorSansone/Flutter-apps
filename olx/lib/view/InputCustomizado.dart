c3b674ebe71100ead5e89df9699e8e97bdf751e9
Ygor Sansone <ygorsansone@gmail.com>
2020-10-19 22:12:18 -0300

    input customizado

---
+++ olx/lib/view/InputCustomizado.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class InputCustomizado extends StatelessWidget {
+  final TextEditingController controller;
+  final String hint;
+  final bool obscure;
+  final bool autofocus;
+  final TextInputType type;
+  InputCustomizado({
+    @required this.controller,
+    @required this.hint,
+    this.obscure = false,
+    this.autofocus = false,
+    this.type = TextInputType.text,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: this.controller,
+        autofocus: this.autofocus,
+        obscureText: this.obscure,
+        keyboardType: this.type,
+        style: TextStyle(fontSize: 20),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+          hintText: this.hint,
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+
+        ));
+  }
+}
